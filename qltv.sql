ab9b8dabe19e0b4fc4ed047e4f444b2d4048e085
Nhu <nhuquynh.nqq@gmail.com>
2019-10-07 10:11:16 +0700

    Load tu sach va crud book author publisher

--- qltv.sql
+++ qltv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 06, 2019 at 05:30 PM
+-- Generation Time: Oct 07, 2019 at 03:07 AM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `author` (
   `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'ngày tạo',
   `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'ngày cập nhật',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `author`
@@ -48,7 +48,7 @@ INSERT INTO `author` (`id`, `authorname`, `created_at`, `updated_at`) VALUES
 (4, 'Mosquite', NULL, NULL),
 (5, 'Nguyễn Văn A', NULL, NULL),
 (6, 'ABC', NULL, NULL),
-(7, 'BCD', NULL, NULL),
+(7, 'Nguy?n Công Chính', '2019-10-07 09:29:45', '2019-10-07 09:29:45'),
 (8, 'Phạm Huy Hoàng', NULL, NULL),
 (9, 'Stiteg Larsson', NULL, NULL),
 (10, 'Robert', NULL, NULL),
@@ -104,14 +104,14 @@ CREATE TABLE IF NOT EXISTS `books` (
   `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `number` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `books`
 --
 
 INSERT INTO `books` (`id`, `id_author`, `id_publisher`, `bookname`, `publish_year`, `description`, `page_number`, `status`, `created_at`, `updated_at`, `image`, `type`, `number`) VALUES
-(1, 1, 1, '1001 Bí Ẩn', NULL, NULL, 0, 0, '2019-10-06 18:04:56', '2019-10-06 18:04:56', 'images/1001bian.jpg', 'Khoa Học - Kỹ Thuật', 3),
+(1, 1, 1, '1001 Bí ?n', NULL, NULL, 0, 0, '2019-10-07 07:54:10', '2019-10-07 07:54:10', 'images/1001bian.jpg', 'Khoa H?c - K? Thu?t', 3),
 (2, 2, 2, 'Án Mạng Trên Sông Nile', NULL, NULL, 0, 0, '2019-10-06 18:05:12', '2019-10-06 18:05:12', 'images/anmangtrensongnile.jpg', 'Trinh Thám - Hình Sự', 5),
 (3, 3, 3, 'Bách Khoa Thư', NULL, NULL, 0, 0, NULL, NULL, '/images/bachkhoathu.jpg', 'Khoa Học - Kỹ Thuật', 2),
 (4, 4, 4, 'Bàn Về Tinh Thần Pháp Luật', NULL, NULL, 0, 0, NULL, NULL, '/images/banvetinhthanpl.jpg', 'Lịch Sử - Chính Trị', 6),
@@ -169,21 +169,20 @@ CREATE TABLE IF NOT EXISTS `book_user` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cb_gv`
+-- Table structure for table `cardbook`
 --
 
-DROP TABLE IF EXISTS `cb_gv`;
-CREATE TABLE IF NOT EXISTS `cb_gv` (
+DROP TABLE IF EXISTS `cardbook`;
+CREATE TABLE IF NOT EXISTS `cardbook` (
+  `MaTheMuon` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `MaBanDoc` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `HoTen` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `GioiTinh` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `NgaySinh` date NOT NULL,
-  `DiaChi` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `PhongBan` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ChucVu` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`MaBanDoc`)
+  `NgayMuon` date NOT NULL,
+  `SoNgayDuocMuon` int(11) NOT NULL,
+  `NgayTraDuKien` int(11) NOT NULL,
+  `NgayTraThucTe` int(11) NOT NULL,
+  `SoNgayMuonTiep` int(11) NOT NULL,
+  `TinhTrang` int(11) NOT NULL,
+  PRIMARY KEY (`MaTheMuon`,`MaBanDoc`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -195,18 +194,18 @@ CREATE TABLE IF NOT EXISTS `cb_gv` (
 DROP TABLE IF EXISTS `publisher`;
 CREATE TABLE IF NOT EXISTS `publisher` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `publisher_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'tên nhà xuất bản',
+  `publishername` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'tên nhà xuất bản',
   `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'ngày tạo',
   `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'ngày cập nhật',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `publisher`
 --
 
-INSERT INTO `publisher` (`id`, `publisher_name`, `created_at`, `updated_at`) VALUES
-(1, 'NXB Hải Phòng', NULL, NULL),
+INSERT INTO `publisher` (`id`, `publishername`, `created_at`, `updated_at`) VALUES
+(1, ' H?i Phòng', '2019-10-07 10:03:38', '2019-10-07 10:03:38'),
 (2, 'NXB Trẻ', NULL, NULL),
 (3, 'NXB Kim Đồng', NULL, NULL),
 (4, 'NXB Chính Trị - Hành Chính', NULL, NULL),
@@ -223,139 +222,17 @@ INSERT INTO `publisher` (`id`, `publisher_name`, `created_at`, `updated_at`) VAL
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sach`
---
-
-DROP TABLE IF EXISTS `sach`;
-CREATE TABLE IF NOT EXISTS `sach` (
-  `MaSach` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `TenSach` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `TacGia` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `NXB` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `TheLoaiSach` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Hinh` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`MaSach`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `sach`
---
-
-INSERT INTO `sach` (`MaSach`, `TenSach`, `TacGia`, `NXB`, `TheLoaiSach`, `Hinh`) VALUES
-('MS01', '1001 Bí Ẩn', 'Duy Tân', 'NXB Hải Phòng', 'Khoa Học - Kỹ Thuật', '/images/1001bian.jpg'),
-('MS02', 'Án Mạng Trên Sông Nile', 'Christie', 'NXB Trẻ', 'Trinh Thám - Hình Sự', '/images/anmangtrensongnile.jpg'),
-('MS03', 'Bách Khoa Thư', 'Bách Khoa', 'NXB Kim Đồng', 'Khoa Học - Kỹ Thuật', '/images/bachkhoathu.jpg'),
-('MS04', 'Bàn Về Tinh Thần Pháp Luật', 'Mosquite', 'NXB Chính Trị - Hành Chính', 'Lịch Sử - Chính Trị', '/images/banvetinhthanpl.jpg'),
-('MS05', 'Bước Chân Theo Dấu Mặt Trời', 'Nguyễn Văn A', 'NXB Trẻ', 'Văn Hóa - Tôn Giáo', '/images/buoctheodaumattroi.jpg'),
-('MS06', 'Chuyến Bay Cuộc Đời', 'ABC', 'NXB Phụ Nữ', 'Tâm Lý - Kỹ Năng Sống', '/images/chuyenbaycuocdoi.jpg'),
-('MS07', 'Chuyên Ngành Cơ Khí', 'BCD', 'NXB Kim Đồng', 'Khoa Học - Kỹ Thuật', '/images/chuyennganhcokhi.jpg'),
-('MS08', 'Code Dạo Ký Sự', 'Phạm Huy Hoàng', 'NXB Đồng Nai', 'Khoa Học - Kỹ Thuật', '/images/codedaokisu.jpg'),
-('MS09', 'Cô Gái Có Hình Xăm Rồng', 'Stiteg Larsson', 'NXB Phụ Nữ', 'Trinh Thám - Hình Sự', '/images/cogaicohinhxamrong.jpg'),
-('MS10', 'Con Tằm', 'Robert', 'NXB Trẻ', 'Trinh Thám - Hình Sự', '/images/contam.jpg'),
-('MS11', 'Điệp Viên Kỳ Quái', 'Nguyệt Tri', 'NXB Kim Đồng', 'Trinh Thám - Hình Sự', 'images/diepvienkiquai.jpg'),
-('MS12', 'Đi Vào Nghiên Cứu Khoa Học', 'Nguyễn Văn Tuấn', 'NXB Tổng Hợp TPHCM', 'Khoa Học - Kỹ Thuật', 'images/divaonghiencuukh.jpg'),
-('MS13', 'Đọc Vị Bất Kì Ai', 'David', 'NXB Trẻ', 'Tâm Lý - Kỹ Năng Sống', 'images/docvibatkiai.jpg'),
-('MS14', 'Đông Kinh Nghĩa Thục', 'Nguyễn Hiễn Lê', 'NXB Văn Hóa Thông Tin', 'Văn Hóa - Tôn Giáo', 'images/dongkinhnghiathuc.jpg'),
-('MS15', 'Kỹ Thuật Lập Trình', 'Trần Đan Thư', 'NXB Khoa Học Kỹ Thuật', 'Khoa Học - Kỹ Thuật', 'images/kythuatlaptrinh.jpg'),
-('MS16', 'Làng Xóm Việt Nam', 'Toan Ánh', 'NXB Văn Hóa Thông Tin', 'Văn Hóa - Tôn Giáo', 'images/langxomvn.jpg'),
-('MS17', 'Giáo Trình Lịch Sử Đảng Cộng Sản Việt Nam', 'BHT', 'NXB Chính Trị Quốc Gia', 'Lịch Sử - Chính Trị', 'images/lichsudang,jpg'),
-('MS18', 'Lịch Sử Quân Sự', 'Nguyễn Văn D', 'NXB Trẻ', 'Lịch Sử - Chính Trị', 'images/líchuquansu.jpg'),
-('MS19', 'Lịch Sử Triết Học', 'Dagobert', 'NXB Văn Hóa Thông Tin', 'Lịch Sử - Chính Trị', 'images/lichsutriethoc.jpg'),
-('MS20', 'Lịch Sử Kháng Chiến Chống Mỹ Cứu Nước', 'Bộ Quốc Phòng', 'NXB Chính Trị Quốc Gia', 'Lịch Sử - Chính Trị', 'images/lskhangchienchongmy.jpg'),
-('MS21', 'Lửa Hận', 'Kelly', 'NXB Trẻ', 'Trinh Thám - Hình Sự', 'images/luahan.jpg'),
-('MS22', 'Mật Mã Davici', 'Dan Brown', 'NXB Văn Hóa Thông Tin', 'Trinh Thám - Hình Sự', 'images/matmadavici.jpg'),
-('MS23', 'Nguồn Gốc Văn Hóa Việt Nam', 'Kim Định', 'NXB Văn Hóa Thông Tin', 'Văn Hóa - Tôn Giáo', 'images/nguongocvanhoavn.jpg'),
-('MS24', 'Nho Giáo Trung Quốc', 'Nguyễn Tôn Nhạn', 'NXB Văn Hóa Thông Tin', 'Văn Hóa - Tôn Giáo', 'images/nhogiaotq.jpg'),
-('MS25', 'Phật Giáo Hòa Hảo', 'Nhiều Tác Giả', 'NXB Tổng Hợp TPHCm', 'Văn Hóa - Tôn Giáo', 'images/phatgiaohoahao.jpg'),
-('MS26', 'Chính Trị Thế Giới Sau Năm 1945', 'Peter Ressi', 'NXB Lao Động', 'Lịch Sử - Chính Trị', 'images/sach-chinh-tri-the-gioi-sau-nam-1945.jpg'),
-('MS27', 'SherlockHomes', 'Conan Doyle', 'NXB Trẻ', 'Trinh Thám - Hình Sự', 'images/Sherlockhome.jpg'),
-('MS28', 'Sự Im Lặng Của Bầy Cừu', 'Thomas Hasson', 'NXB Trẻ', 'Trinh Thám - Hình Sự', 'images/suimlangcuabaycuu.jpg'),
-('MS29', 'Tâm Lý Học Đám Đông', 'Gustave', 'NXB Trẻ', 'Tâm Lý - Kỹ Năng Sống', 'images/tamlyhocdamdong.jpg'),
-('MS30', 'Tâm Thức Isreal', 'Alon Gratch', 'NXB Trẻ', 'Tâm Lý - Kỹ Năng Sống', 'images/tamthucistreal.jpg'),
-('MS31', 'Triết Học Kì Thú', 'Trần Văn D', 'NXB Văn Hóa Thông Tin', 'Triết Học', 'images/thkithu.jpg'),
-('MS32', 'Triết Học Luật Pháp', 'Raymond Wacks ', 'NXB Tri Thức', 'Triết Học', 'images/thluatphap.jpg'),
-('MS33', 'Giáo Trình Triết Học Mác Lê-nin', 'Bộ Giáo Dục ', 'NXB Chính Trị Quốc Gia', 'Triết Học', 'images/thmac.jpg'),
-('MS34', 'Triết Sử Ấn Độ', 'Hoành Sơn', 'NXB Kim Đồng', 'Triết Học', 'images/thando.jpg'),
-('MS35', 'Tôi Tự Học', 'Thu Giang', 'NXB Trẻ', 'Tâm Lý - Kỹ Năng Sống', 'images/toituhoc.jpg'),
-('MS36', 'Bên Nhau Trọn Đời', 'Cố Mạn', 'NXB Trẻ', 'Tiểu Thuyết', 'images/ttbennhautrondoi.jpg'),
-('MS37', 'Chúa Tể Những Chiếc Nhẫn', 'Tokien', 'NXB Trẻ', 'Tiểu Thuyết', 'images/ttchuatenhungchiecnhan.jpg'),
-('MS38', 'Lâu Đài Cát', 'Madalena', 'NXB Trẻ', 'Tiểu Thuyết', 'images/ttlaudaicat.jpg'),
-('MS39', 'Miền Trần Gian', 'Dương Kỳ Anh', 'NXB Hội Nhà Văn', 'Tiểu Thuyết', 'images/ttmientrangian.jpg'),
-('MS40', 'Sông', 'Nguyễn Ngoc Tứ', 'NXB Trẻ', 'Tiểu Thuyết', 'images/ttsong.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sinhvien`
---
-
-DROP TABLE IF EXISTS `sinhvien`;
-CREATE TABLE IF NOT EXISTS `sinhvien` (
-  `MaBanDoc` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `HoTen` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `GioiTinh` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `DiaChi` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `MaKhoa` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Lop` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `NgaySinh` date NOT NULL,
-  `Username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`MaBanDoc`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `sinhvien`
---
-
-INSERT INTO `sinhvien` (`MaBanDoc`, `HoTen`, `GioiTinh`, `DiaChi`, `MaKhoa`, `Lop`, `NgaySinh`, `Username`, `Password`) VALUES
-('SV01', 'Nguyễn Văn A', 'Nam', 'Thành Tây', 'CNTT', 'C', '1997-10-02', 'NVA', 'NVA');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `themuonsach`
---
-
-DROP TABLE IF EXISTS `themuonsach`;
-CREATE TABLE IF NOT EXISTS `themuonsach` (
-  `MaTheMuon` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `MaBanDoc` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `NgayMuon` date NOT NULL,
-  `SoNgayDuocMuon` int(11) NOT NULL,
-  `NgayTraDuKien` int(11) NOT NULL,
-  `NgayTraThucTe` int(11) NOT NULL,
-  `SoNgayMuonTiep` int(11) NOT NULL,
-  `TinhTrang` int(11) NOT NULL,
-  PRIMARY KEY (`MaTheMuon`,`MaBanDoc`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `thuthu`
---
-
-DROP TABLE IF EXISTS `thuthu`;
-CREATE TABLE IF NOT EXISTS `thuthu` (
-  `MaThuThu` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `HoTen` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `NgaySinh` date NOT NULL,
-  `GioiTinh` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `DiaChi` int(30) NOT NULL,
-  PRIMARY KEY (`MaThuThu`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `Username` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`Username`)
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`iduser`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `password` (`password`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -368,12 +245,13 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'email',
-  `password` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'mật khẩu',
+  `phone` int(11) DEFAULT NULL COMMENT 'sdt',
   `firstname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'họ',
   `lastname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'tên',
   `role` tinyint(4) DEFAULT '0' COMMENT 'quyền(0 : đọc giả, 1 : GV_CB, 2: thủ thư(admin) )',
   `created_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'ngày tạo',
   `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'ngày cập nhật',
+  `account` int(8) NOT NULL COMMENT 'tài khoản dùng để xác thực danh tính vd sv thì là tk sinh viên',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 COMMIT;
